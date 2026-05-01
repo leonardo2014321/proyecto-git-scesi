875282a3489cc14e71a1832992b5e4380b41a954
Leonardo Mena Morante <leonardomenamorante@gmail.com>
2026-05-01 17:51:23 -0400

    feat: wire imports and initial render to app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 import { resources } from "./data/resources.js";
+import { createSection } from "./components/section.js";
+import { renderNavbar } from "./components/navbar.js";
+import { applyFilters } from "./features/filter.js";
+import { getFavorites } from "./features/favorites.js";
+import { initTheme, toggleTheme } from "./features/theme.js";
 
 const state = {
   searchText: "",
   activeCategory: "all",
-  showFavOnly: false,
+  showFavOnly: false
 };
 
 const categories = Object.keys(resources);
 
 function renderResources() {
   const app = document.getElementById("app");
-
-  if (!app) return;
-
-  app.innerHTML = "<p>Loading...</p>";
+  app.innerHTML = "";
 }
